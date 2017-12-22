36e0f7cfc3379e86f3b08700e269f83010c8a530
Angelika <angelikapostar@gmail.com>
2017-12-22 19:36:44 +0100

    Finish part 2

--- day15/solution.R
+++ day15/solution.R
@@ -24,3 +24,21 @@ for (i in 1:40000000) {
     if (matching(bits)) match_count <- match_count + 1
 }
 print(match_count)
+
+
+# --- Part Two ---
+
+next_satisfying_value <- function(generator, condition) {
+    generator$value <- next_value(generator)
+    while(generator$value %% condition) generator$value <- next_value(generator)
+    return(generator$value)
+}
+
+picky_count <- 0
+for (i in 1:5000000) {
+    generator_A$value <- next_satisfying_value(generator_A, 4)
+    generator_B$value <- next_satisfying_value(generator_B, 8)
+    if (matching(list(to_bit(generator_A$value), to_bit(generator_B$value)))) picky_count <- picky_count + 1
+    
+}
+print(picky_count)
